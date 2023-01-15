365c123c96a76d91055353cd21903fac1eb33d39
Asosan <ka72suiden@icloud.com>
2023-01-15 14:23:05 +0900

    修正

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Fresno Printer for Raspberry Pi zero 
+import sys
+sys.path.append('/home/aso2023/.local/lib/python3.9/site-packages')
 # escpos
 from escpos.printer import Usb
 from escpos import *
@@ -32,51 +34,51 @@ def createReceipt(doc):
     paid = data['paid']
     orderlist = data['orders']
     # head part: タイトル周辺
-    head = Image.new('1', (width, 840), 255)
+    head = Image.new('1', (width, 210), 255)
     drawhead = ImageDraw.Draw(head)
-    drawhead.text((0,160), "Navy Bottle Coffee Kyoto", font=titlefont , fill=0)
-    drawhead.text((0,320), "東京都世田谷区池尻4-7-1", anchor="ls", font=textfont , fill=0)
-    drawhead.text((0,384), "筑駒高3喫茶班",  anchor="ls", font=textfont , fill=0)
-    drawhead.text((width,320), date, anchor="ls", font=numberfont , fill=0)
-    drawhead.text((width,384), time,  anchor="ls", font=numberfont , fill=0)
-    drawhead.text((width/2,480), "お支払い", anchor="ms", font=textfont , fill=0)
-    drawhead.line(((0,520), (width, 130)), fill=0, width=12)
-    drawhead.text((0,576), "伝票名：", font=textfont , fill=0)
-    drawhead.text((0,640), "レシート番号：", font=textfont , fill=0)
-    drawhead.text((200,576), name, font=textfont , fill=0)
-    drawhead.text((344,640), num, font=textfont , fill=0)
-    drawhead.line(((0, 704), (width, 704)), fill=0, width=4)
-    drawhead.text((width/2,480), type, anchor="ml", font=textfont, fill=0)
-    drawhead.line(((0, 800), (width, 800)), fill=0, width=4)
+    drawhead.text((0,40), "Navy Bottle Coffee Kyoto", font=titlefont , fill=0)
+    drawhead.text((0,80), "東京都世田谷区池尻4-7-1", anchor="ls", font=textfont , fill=0)
+    drawhead.text((0,96), "筑駒高3喫茶班",  anchor="ls", font=textfont , fill=0)
+    drawhead.text((width,80), date, anchor="ls", font=numberfont , fill=0)
+    drawhead.text((width,96), time,  anchor="ls", font=numberfont , fill=0)
+    drawhead.text((width/2,120), "お支払い", anchor="ms", font=textfont , fill=0)
+    drawhead.line(((0,130), (width, 130)), fill=0, width=3)
+    drawhead.text((0,144), "伝票名：", font=textfont , fill=0)
+    drawhead.text((0,160), "レシート番号：", font=textfont , fill=0)
+    drawhead.text((50,144), name, font=textfont , fill=0)
+    drawhead.text((86,160), num, font=textfont , fill=0)
+    drawhead.line(((0, 176), (width, 176)), fill=0, width=1)
+    drawhead.text((width/2,190), type, anchor="lm", font=textfont, fill=0)
+    drawhead.line(((0, 200), (width, 200)), fill=0, width=4)
     # body part: 項目の部分
-    height = 200 * len(orderlist)
+    height = 50 * len(orderlist)
     body = Image.new('1', (width, height), 255)
     bodyhead = ImageDraw.Draw(body)
     for index, order in enumerate(orderlist):
         l = order.split(',')
-        bodyhead.text((0, index*200 + 40), l[0] + " × " + str(l[1]), anchor="lm", font=textfont, fill=0)
-        bodyhead.text((width, index*200 + 40), "¥"+str(l[2]), anchor="rm", font=numberfont, fill=0)
-        bodyhead.text((0, index*200 + 112), l[3], anchor="lm", font=textfont, fill=0)
+        bodyhead.text((0, index*50 + 10), l[0] + " × " + str(l[1]), anchor="lm", font=textfont, fill=0)
+        bodyhead.text((width, index*50 + 10), "¥"+str(l[2]), anchor="rm", font=numberfont, fill=0)
+        bodyhead.text((0, index*50 + 28), l[3], anchor="lm", font=textfont, fill=0)
 
     # foot part: 合計金額のあたり
-    foot = Image.new('1', (width, 360), 255)
+    foot = Image.new('1', (width, 90), 255)
     foothead = ImageDraw.Draw(foot)
-    foothead.line(((0, 24), (width, 24)), fill=0, width=12)
-    foothead.text((0,80), "合計", anchor="lm", font=textfont, fill=0)
-    foothead.text((width,80), "¥"+str(sum), anchor="rm", font=largefont, fill=0)
-    foothead.text((0,176), "現金", anchor="lm", font=textfont, fill=0)
-    foothead.text((width,174), "¥"+str(paid), anchor="rm", font=largefont, fill=0)
-    foothead.text((0,256), "お釣り", anchor="lm", font=textfont, fill=0)
-    foothead.text((width,256), "¥"+str(paid-sum), anchor="rm", font=largefont, fill=0)
+    foothead.line(((0, 6), (width, 6)), fill=0, width=3)
+    foothead.text((0,20), "合計", anchor="lm", font=textfont, fill=0)
+    foothead.text((width,20), "¥"+str(sum), anchor="rm", font=largefont, fill=0)
+    foothead.text((0,44), "現金", anchor="lm", font=textfont, fill=0)
+    foothead.text((width,44), "¥"+str(paid), anchor="rm", font=largefont, fill=0)
+    foothead.text((0,64), "お釣り", anchor="lm", font=textfont, fill=0)
+    foothead.text((width,64), "¥"+str(paid-sum), anchor="rm", font=largefont, fill=0)
     # Print
-    #p = Usb(0x0416, 0x5011, 0)
-    #p.image(head)
-    #p.image(body)
-    #p.image(foot)
-    #p.cut()
-    head.save("head.jpg")
-    body.save("body.jpg")
-    foot.save("foot.jpg")
+    p = Usb(0x0416, 0x5011, 0)
+    p.image(head)
+    p.image(body)
+    p.image(foot)
+    p.cut()
+    #head.save("head.jpg")
+    #body.save("body.jpg")
+    #foot.save("foot.jpg")
     db.collection("ReceiptData").document(num).delete()
 
 
@@ -97,11 +99,11 @@ watcher = ref.on_snapshot(onUpdate)
 
 
 # Print Settings
-width = 1600
-titlefont = ImageFont.truetype('ZenKakuGothicNew-Medium.ttf', 128, encoding='unic')
-largefont = ImageFont.truetype('ZenKakuGothicNew-Medium.ttf', 80, encoding='unic') # 合計金額
-numberfont = ImageFont.truetype('ZenKakuGothicNew-Medium.ttf', 64, encoding='unic')
-textfont = ImageFont.truetype('ZenKakuGothicNew-Regular.ttf', 64, encoding='unic')
+width = 384
+titlefont = ImageFont.truetype('ZenKakuGothicNew-Medium.ttf', 26, encoding='unic')
+largefont = ImageFont.truetype('ZenKakuGothicNew-Medium.ttf', 16, encoding='unic') # 合計金額
+numberfont = ImageFont.truetype('ZenKakuGothicNew-Medium.ttf', 13, encoding='unic')
+textfont = ImageFont.truetype('ZenKakuGothicNew-Regular.ttf', 12, encoding='unic')
 
 
 while True:
